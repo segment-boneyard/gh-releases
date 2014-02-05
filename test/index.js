cb291178d8a48785d0720d4400e9dd235a7c8112
TJ Holowaychuk <tj@vision-media.ca>
2014-02-05 07:45:07 -0800

    add .token test back

--- test/index.js
+++ test/index.js
@@ -2,18 +2,18 @@
 var releases = require('..');
 
 describe('releases(opts, fn)', function(){
-  // it('should respond with private releases via .token', function(done){
-  //   releases({
-  //     token: process.env.TOKEN,
-  //     repo: 'segmentio/accounts'
-  //   }, function(err, releases){
-  //     if (err) return done(err);
-  //     releases.should.not.be.empty;
-  //     releases[0].should.have.property('name');
-  //     releases[0].should.have.property('commit');
-  //     done();
-  //   });
-  // })
+  it('should respond with private releases via .token', function(done){
+    releases({
+      token: process.env.TOKEN,
+      repo: 'segmentio/accounts'
+    }, function(err, releases){
+      if (err) return done(err);
+      releases.should.not.be.empty;
+      releases[0].should.have.property('name');
+      releases[0].should.have.property('commit');
+      done();
+    });
+  })
 
   it('should respond with private releases via .user / .pass', function(done){
     releases({
